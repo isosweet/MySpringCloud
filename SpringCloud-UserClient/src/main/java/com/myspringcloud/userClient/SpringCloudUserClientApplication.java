3dc0d50c09c62ef67599dae2e98fd4ec5728f03a
netant <405438526@qq.com>
2017-05-02 20:49:40 +0800

    将服务注册到Eureka上

--- SpringCloud-UserClient/src/main/java/com/myspringcloud/userClient/SpringCloudUserClientApplication.java
+++ SpringCloud-UserClient/src/main/java/com/myspringcloud/userClient/SpringCloudUserClientApplication.java
@@ -2,8 +2,10 @@ package com.myspringcloud.userClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudUserClientApplication {
 
 	public static void main(String[] args) {
