b04b478a82d57f26b101dc2e2cfa649e7b5f2d99
netant <405438526@qq.com>
2017-05-02 20:36:44 +0800

    用户微服务客户端

---
+++ SpringCloud-UserClient/src/main/java/com/myspringcloud/userClient/SpringCloudUserClientApplication.java
@@ -0,0 +1,12 @@
+package com.myspringcloud.userClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudUserClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudUserClientApplication.class, args);
+	}
+}
