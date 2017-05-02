e776483415a076085c9d5284a964a3213f3784b7
netant <405438526@qq.com>
2017-05-02 20:43:05 +0800

    用户微服务服务端

---
+++ SpringCloud-UserServcie/src/main/java/com/myspringcloud/userService/SpringCloudUserServcieApplication.java
@@ -0,0 +1,12 @@
+package com.myspringcloud.userService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudUserServcieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudUserServcieApplication.class, args);
+	}
+}
