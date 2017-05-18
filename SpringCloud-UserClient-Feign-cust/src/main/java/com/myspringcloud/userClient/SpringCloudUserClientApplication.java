b02409a1a58d566ae4d05156ad112f31d981452e
netant <405438526@qq.com>
2017-05-18 21:09:28 +0800

    cust

---
+++ SpringCloud-UserClient-Feign-cust/src/main/java/com/myspringcloud/userClient/SpringCloudUserClientApplication.java
@@ -0,0 +1,16 @@
+package com.myspringcloud.userClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class SpringCloudUserClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudUserClientApplication.class, args);
+	}
+}
